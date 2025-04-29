360f1d0aee62a7251e09e760d41a2e1e454e9644
hide-sudo <deathhide08@gmail.com>
2025-04-29 19:43:41 +0530

    Update info.py

--- devclone/plugins/misc/info.py
+++ devclone/plugins/misc/info.py
@@ -2,7 +2,7 @@ import asyncio, os, time, aiohttp
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
 from asyncio import sleep
-from VIP_INNOCENT import app
+from devclone import app
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from pyrogram.types import *
@@ -66,8 +66,8 @@ async def get_userinfo_img(
 
 # --------------------------------------------------------------------------------- #
 
-bg_path = "VIP_INNOCENT/assets/userinfo.png"
-font_path = "VIP_INNOCENT/assets/hiroko.ttf"
+bg_path = "devclone/assets/userinfo.png"
+font_path = "devclone/assets/hiroko.ttf"
 
 # --------------------------------------------------------------------------------- #
 
