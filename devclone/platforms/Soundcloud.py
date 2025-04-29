bdfcba60a7859b22bd199bb4eb239e7462c2188d
hide-sudo <deathhide08@gmail.com>
2025-04-29 15:49:17 +0530

    Update Soundcloud.py

--- devclone/platforms/Soundcloud.py
+++ devclone/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from VIP_INNOCENT.utils.formatters import seconds_to_min
+from devclone.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
