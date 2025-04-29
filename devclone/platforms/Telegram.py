1ac2c7697c17a2d6d9450d23c396bda57779178d
hide-sudo <deathhide08@gmail.com>
2025-04-29 15:49:36 +0530

    Update Telegram.py

--- devclone/platforms/Telegram.py
+++ devclone/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from VIP_INNOCENT import app
-from VIP_INNOCENT.utils.formatters import (
+from devclone import app
+from devclone.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
