1d90ac31645d5e0cea6d2763c995a4a4aaa315fa
hide-sudo <deathhide08@gmail.com>
2025-04-29 19:47:46 +0530

    Update block.py

--- devclone/plugins/sudo/block.py
+++ devclone/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from VIP_INNOCENT import app
-from VIP_INNOCENT.misc import SUDOERS
-from VIP_INNOCENT.utils.database import add_gban_user, remove_gban_user
-from VIP_INNOCENT.utils.decorators.language import language
-from VIP_INNOCENT.utils.extraction import extract_user
+from devclone import app
+from devclone.misc import SUDOERS
+from devclone.utils.database import add_gban_user, remove_gban_user
+from devclone.utils.decorators.language import language
+from devclone.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
