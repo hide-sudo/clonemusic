1d5a908e5edb0d8908b3d528c3600c3afa3b8f86
hide-sudo <deathhide08@gmail.com>
2025-04-29 15:56:58 +0530

    Update mustjoin.py

--- devclone/plugins/bot/mustjoin.py
+++ devclone/plugins/bot/mustjoin.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from VIP_INNOCENT import app
+from devclone import app
 
 #--------------------------
 
