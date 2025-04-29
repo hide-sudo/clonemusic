62dc91a62ebf32e3750531c329ad8b4bb045e4a3
hide-sudo <deathhide08@gmail.com>
2025-04-29 19:44:07 +0530

    Update seeker.py

--- devclone/plugins/misc/seeker.py
+++ devclone/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from VIP_INNOCENT.misc import db
-from VIP_INNOCENT.utils.database import get_active_chats, is_music_playing
+from devclone.misc import db
+from devclone.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
