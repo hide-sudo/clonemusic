091ba9e06b7d042cc7100b2ba01eb23d1c0ca12a
hide-sudo <deathhide08@gmail.com>
2025-04-29 15:49:49 +0530

    Update Youtube.py

--- devclone/platforms/Youtube.py
+++ devclone/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from VIP_INNOCENT.utils.database import is_on_off
-from VIP_INNOCENT.utils.formatters import time_to_seconds
+from devclone.utils.database import is_on_off
+from devclone.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
