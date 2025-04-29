eb0a6f3f540c2a293bb77e8164067b07390cf100
hide-sudo <deathhide08@gmail.com>
2025-04-29 19:49:13 +0530

    Update Gpt.py

--- devclone/plugins/tools/Gpt.py
+++ devclone/plugins/tools/Gpt.py
@@ -1,5 +1,5 @@
 import requests
-from VIP_INNOCENT import app
+from devclone import app
 from pyrogram.types import Message
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
