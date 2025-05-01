bcb71b18374312b6c0a146d3c2322fa7a19e2e7e
hide-sudo <deathhide08@gmail.com>
2025-05-01 20:38:00 +0530

    Update bot.py

--- devclone/core/bot.py
+++ devclone/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class INNOCENT(Client):
+class dev(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
