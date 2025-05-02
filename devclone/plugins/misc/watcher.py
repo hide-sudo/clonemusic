4ca7792a8b2f6feb9d662348f0ab77e79c5b1573
hide-sudo <deathhide08@gmail.com>
2025-05-02 09:12:34 +0530

    Update watcher.py

--- devclone/plugins/misc/watcher.py
+++ devclone/plugins/misc/watcher.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from devclone import app
-from devclone.core.call import INNOCENT
+from devclone.core.call import dev
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await INNOCENT.stop_stream_force(message.chat.id)
+    await dev.stop_stream_force(message.chat.id)
