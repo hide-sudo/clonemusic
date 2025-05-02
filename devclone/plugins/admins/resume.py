a477386238c9a2f50aea60ab4c521e01ee1cb3d5
hide-sudo <deathhide08@gmail.com>
2025-05-02 09:03:52 +0530

    Update resume.py

--- devclone/plugins/admins/resume.py
+++ devclone/plugins/admins/resume.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from devclone import app
-from devclone.core.call import INNOCENT
+from devclone.core.call import dev
 from devclone.utils.database import is_music_playing, music_on
 from devclone.utils.decorators import AdminRightsCheck
 from devclone.utils.inline import close_markup
@@ -15,7 +15,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await INNOCENT.resume_stream(chat_id)
+    await dev.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
